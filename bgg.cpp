37e0f91f10a393c31118d188f66a3b3c2d4c564a
F74036263 <ads841220@gmail.com>
2015-03-16 23:30:23 +0800

    Create bgg.cpp

---
+++ bgg.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	while(1)
+	{
+		cout<<n;
+		cout<<"\n";
+		if(n==1)
+		  break;
+		else if(n%2!=0)
+		     n=3*n+1;
+		else
+		     n=n/2;
+			      
+		  
+	}
+	return 0;
+}
